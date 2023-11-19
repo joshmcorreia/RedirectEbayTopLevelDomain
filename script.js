99e6f60bfa6b686280c0421436c4014b19a58678
Josh Correia <josh.correia@kove.com>
2023-11-18 17:45:39 -0800

    Make script work with all top level domains

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // ==UserScript==
-// @name         Redirect eBay Canada to eBay USA
-// @description  Redirects eBay Canada URLs to eBay USA URLs. Useful for when you get eBay links from friends in Canada and want to see the USA version of eBay.
+// @name         Redirect eBay Top Level Domain
+// @description  Redirects eBay URLs to your desired location. Useful for when you get eBay links from friends in other countries and want to see your local version.
 // @author       joshmcorreia
-// @match        *://*.ebay.ca/*
+// @match        *://www.ebay.*/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
+var desired_top_level_domain = ".com"; // CHANGE ME TO REFLECT YOUR DESIRED DOMAIN
+
+const url_regex = /(?<=ebay).*?(?=\/)/
+
 var old_url_path = window.location.href;
-var ebay_canada_string = "ebay.ca";
-var ebay_usa_string = "ebay.com";
-if (old_url_path.includes(ebay_canada_string)) {
-  var new_url_path = old_url_path.replace(ebay_canada_string, ebay_usa_string);
+var matches = old_url_path.match(url_regex);
+if (!matches.includes(desired_top_level_domain)) {
+  var new_url_path = old_url_path.replace(url_regex, desired_top_level_domain);
   window.location.replace(new_url_path);
 }
